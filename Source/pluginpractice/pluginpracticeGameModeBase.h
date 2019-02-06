8038a3542ffc41bccf297918006e91354a2d0072
gsansone4 <gsansone4@gmail.com>
2019-02-05 19:32:30 -0500

    basic plugin code

---
+++ Source/pluginpractice/pluginpracticeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "pluginpracticeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLUGINPRACTICE_API ApluginpracticeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
